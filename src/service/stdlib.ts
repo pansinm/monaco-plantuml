1b7995ad3aa106b6c0191d13c7dbe991b0b3f667
pansinm <pansinm@foxmail.com>
2025-06-17 19:16:13 +0800

    chore:  使用固定stdlib版本,以防目录变更

--- src/service/stdlib.ts
+++ src/service/stdlib.ts
@@ -1,8 +1,10 @@
 import { DEFAULT_EXPIRES } from "./constants";
 import { getJson } from "./fetcher";
 
+type TreeItem = { path: string; type: string; url: string };
+
 class Stdlib {
-  modules: { path: string; type: string; url: string }[] = [];
+  modules: TreeItem[] = [];
   loading = false;
 
   getModule(name: string) {
@@ -20,15 +22,17 @@ class Stdlib {
     }
     this.loading = true;
     return getJson(
-      "https://api.github.com/repos/plantuml/plantuml-stdlib/git/trees/master?recursive=10"
+      "https://api.github.com/repos/plantuml/plantuml-stdlib/git/trees/b88251729ceac046eaa12654e87a0c81721cf6b0?recursive=10"
     )
       .then((body) => {
         if (body?.tree) {
           this.modules =
-            (body as any).tree?.map((item: any) => ({
-              ...item,
-              path: item.path.replace(/^stdlib\//, ""),
-            })) || [];
+            (body as any).tree
+              ?.filter((item: TreeItem) => /^stdlib/.test(item.path))
+              .map((item: TreeItem) => ({
+                ...item,
+                path: item.path.replace(/^stdlib\//, ""),
+              })) || [];
         }
         return this.modules;
       })
